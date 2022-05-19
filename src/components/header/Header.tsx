adeb096cbba1a29f71e2e49384507ca6b0dcd70d
lilgvr <71455385+lilgvr@users.noreply.github.com>
2022-05-19 14:30:52 +0400

    header hover changes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,57 +1,42 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import "./header.scss";
-import {infiniteGenerator} from "../../service/functions";
-
-interface IHeaderItem {
-    id: number,
-    title: string,
-    scrollEl: string
-}
 
 const Header: FC = () => {
-    const idGenerator = infiniteGenerator();
-    const headerItems: IHeaderItem[] = [
-        {
-            id: idGenerator.next().value,
-            title: 'Home',
-            scrollEl: 'home-block--ctr'
-        },
-        {
-            id: idGenerator.next().value,
-            title: 'About me',
-            scrollEl: 'about-block'
-        },
-        {
-            id: idGenerator.next().value,
-            title: 'Skills',
-            scrollEl: 'skills-block--ctr'
-        },
-        {
-            id: idGenerator.next().value,
-            title: 'Portfolio',
-            scrollEl: 'portfolio-block--ctr'
-        },
-        {
-            id: idGenerator.next().value,
-            title: 'Contacts',
-            scrollEl: 'contacts-block'
-        }
-    ];
+    const [hovered, setHovered] = useState(true);
+
+    const mouseEnter = () => {
+        setHovered(false);
+    }
+    const mouseLeave = () => {
+        setHovered(true);
+    }
 
     return (
         <header>
-            {headerItems.map(el => <HeaderItem title={el.title} scrollEl={el.scrollEl} key={el.id}/>)}
+            <HeaderItem title='Home' scrollEl='home-block--ctr' selected={hovered}/>
+            <HeaderItem title='About me' scrollEl='about-block' mouseEnter={mouseEnter} mouseLeave={mouseLeave}/>
+            <HeaderItem title='Skills' scrollEl='skills-block--ctr' mouseEnter={mouseEnter} mouseLeave={mouseLeave}/>
+            <HeaderItem title='Portfolio' scrollEl='portfolio-block--ctr' mouseEnter={mouseEnter}
+                        mouseLeave={mouseLeave}/>
+            <HeaderItem title='Contacts' scrollEl='contacts-block' mouseEnter={mouseEnter} mouseLeave={mouseLeave}/>
         </header>
     );
 };
 
-const HeaderItem: FC<{ title: string, scrollEl: string }> = ({title, scrollEl}) => {
+const HeaderItem: FC<{
+    title: string,
+    scrollEl: string,
+    selected?: boolean,
+    mouseEnter?: () => void,
+    mouseLeave?: () => void
+}> = ({title, scrollEl, selected, mouseEnter, mouseLeave}) => {
     const handleClick = () => {
         document.getElementsByClassName(scrollEl)[0].scrollIntoView({block: "start", behavior: "smooth"});
     }
 
     return (
-        <div className="header-item" onClick={handleClick}>
+        <div className={`header-item${selected ? ' selected' : ''}`} onClick={handleClick} onMouseEnter={mouseEnter}
+             onMouseLeave={mouseLeave}>
             <p>{title}</p>
         </div>
     );

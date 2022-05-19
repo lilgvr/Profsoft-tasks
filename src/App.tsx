67acf95d44c0a3201f2457aa24daa4dec25399ea
lilgvr <71455385+lilgvr@users.noreply.github.com>
2022-05-19 16:31:25 +0400

    post request on start

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useCallback, useEffect, useState} from 'react';
 import './App.css';
 import Header from "./components/header/Header";
 import HomeBlock from "./components/home-block/HomeBlock";
@@ -11,16 +11,46 @@ import MenuContext from "./service/MenuContext";
 import Menu from "./components/home-block/mobile/Menu";
 import HomeBlockMobile from "./components/home-block/mobile/HomeBlockMobile";
 
-
 function App() {
     const [menuOpened, setMenuOpened] = useState(false);
+    const [requestContent, setRequestContent] = useState({});
+
+    const fetchPost = useCallback(async () => {
+        const url = 'https://jsonplaceholder.typicode.com/posts';
+        const title = 'myTitle', body = 'myBody', userId = 543;
+        const params = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({title, body, userId})
+        };
+
+        let response = await fetch(url, params).then(res => {
+            if (!res.ok) {
+                throw new Error(`HTTP-статус: ${res.status}`);
+            }
+            return res.json();
+        }).catch(err => {
+            console.error(err);
+        });
+
+        if (response) {
+            setRequestContent(response);
+            console.log(response);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchPost();
+    }, [fetchPost]);
 
     return (
         <div className="container">
             <Media queries={{
                 desktopOrLaptop: '(min-width: 1224px)',
                 tabletOrMobile: '(max-width: 1224px)',
-                // bigScreen: '(min-width: 1824px)'
             }}>
                 {matches => (
                     <Fragment>
